6c1d3a3f2c46cf76447e33d8bb3511ebf925d26d
Vincent Abesamis <vincentabesamis03@gmail.com>
2025-07-23 12:56:18 +0800

    Add back button

--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -1,13 +1,20 @@
 import MainNav from '@/components/common/main-nav'
 import RecipeForm from '@/components/common/recipe-form'
+import { ArrowLeftIcon } from 'lucide-react'
 import Link from 'next/link'
 
 export default function Create() {
   return (
     <main className="min-w-full min-h-screen bg-gray-200">
-      <MainNav className="w-full justify-between">
-        <Link href="/">Home</Link>
-      </MainNav>
+      <MainNav className="w-full justify-between">Create Recipe</MainNav>
+      <div className="px-10 py-4">
+        <Link
+          href="/"
+          className="text-blue-500 hover:text-blue-600 flex items-center gap-2"
+        >
+          <ArrowLeftIcon className="size-4" /> Back
+        </Link>{' '}
+      </div>
       <RecipeForm />
     </main>
   )
