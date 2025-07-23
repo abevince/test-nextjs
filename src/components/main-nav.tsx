a5f7766f82a07ddc35def9358491879a1747b374
Vincent Abesamis <vincentabesamis03@gmail.com>
2025-07-23 12:41:44 +0800

    Organize components

--- src/components/main-nav.tsx
+++
@@ -1,9 +0,0 @@
-const MainNav = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <nav className="w-full h-16 bg-blue-600 flex justify-end items-center px-10">
-      {children}
-    </nav>
-  )
-}
-
-export default MainNav
